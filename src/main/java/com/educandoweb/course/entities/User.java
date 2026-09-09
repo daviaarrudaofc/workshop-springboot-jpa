15e6cd0f3f0f0b40c82550aed242b5d29644e3ad
davi arruda <daviarruda092@gmail.com>
2026-09-09 18:49:54 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,10 +3,20 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user") // é pra renomear,pois a palavra User é uma palavra reservada do banco de dados H2, ai é para n dar conflito
 public class User implements Serializable{//serve para que o obj trafegue na rede, que possa seja gravado em arquivos
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id // como ela é auto incrementada no database tem que ter o generateVaule
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;

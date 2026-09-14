dc9adc3ba5be11e6844be4c023c13736fe421ab4
davi arruda <daviarruda092@gmail.com>
2026-09-14 09:09:36 -0300

    User insert

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
@@ -31,4 +35,19 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@PostMapping // Para inserir um novo usuário através de uma requisição HTTP POST
+	public ResponseEntity<User> insert(@RequestBody User obj) {// @RequestBody indica que os dados chegam no corpo da requisição em JSON.
+			obj = service.insert(obj);							// O Spring desserializa automaticamente esse JSON para um objeto User do Java.
+			
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest()	// pega a URL da requisição atual, ex: /users
+		            .path("/{id}")										// adiciona /{id} no final da URL
+		            .buildAndExpand(obj.getId())						// substitui {id} pelo ID do usuário criado
+		            .toUri();											// transforma essa URL em um objeto URI
+
+		    return ResponseEntity.created(uri).body(obj);
+			
+			
+	}
+	
 }

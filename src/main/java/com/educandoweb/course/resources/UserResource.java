bb774a5f7b0cac735961f2da2ada02845cdb8aa1
davi arruda <daviarruda092@gmail.com>
2026-09-14 09:31:41 -0300

    User delete

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,8 +47,12 @@ public class UserResource {
 		            .toUri();											// transforma essa URL em um objeto URI
 
 		    return ResponseEntity.created(uri).body(obj);
-			
-			
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();  //o .noConten() vai retornar uma resposta vazia e o codigo HTTP que n tem conteudo é o 204
 	}
 	
 }
